31ae968045e1bd4c123bf08638cc0c180071e1af
MonsterWeed <56317119+MonsterWeed@users.noreply.github.com>
2019-10-09 16:12:16 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const  client = new Discord.Client();
 
 client.on("ready", () => {
    console.log("Estoy listo!");
-   client.user.setGame('Online 24/7');
+   client.user.setGame('!comandos');
   
   
 });
