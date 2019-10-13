8397ada1cc32f7f749a35f3be7bade17400dc1b5
MonsterWeed <56317119+MonsterWeed@users.noreply.github.com>
2019-10-13 14:38:33 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -81,16 +81,17 @@ if(message.content.startsWith("!limpiar")&&message.content.endsWith("!limpiar"))
   }
    
    
- if(message.content.startsWith("!das")){
-    message.channel.send("[LNDC]©                                                  13/10/2019\n\n\n"+
+
+  if(message.content.startsWith("!das")){
+    message.channel.send("[LNDC]©                                                  13/10/2019\n\n"+
     "La administracion se complace en presentar el canal de Updates del servidor.\n\n"
     +"En esta seccion se publicara las modificaciones que se le hagan al servidor.\n\n"
-    +"**Este canal es exclusivamente para publicaciones de la administracion por lo tanto permanecera cerrado(solo lectura).**");
+    +"**Este canal es exclusivamente para publicaciones de la administracion por lo tanto permanecera cerrado(solo lectura).**\n@here");
   }
   if (message.content.startsWith("!asd")){
-    message.channel.send("[LNDC]©                                                  13/10/2019\n\n\n"+
-    "__**Actualizacion v1.1:**__\n\n"+
-    "Nuevo comando de **!limpiar**. Este comando es util para aquellos que desean limpiar el chat cuando alguien manda algun tipo de material comprometido de instagram o twitter dentro del canal de mensajes.");
+    message.channel.send("[LNDC]©                                                  13/10/2019\n\n"+
+    "__**Actualizacion v1.1:**__\n"+
+    "Nuevo comando de **!limpiar**. Este comando es util para aquellos que desean limpiar el chat cuando alguien manda algun tipo de material comprometido de instagram o twitter dentro del canal de mensajes.\n@here");
   }
 
 
