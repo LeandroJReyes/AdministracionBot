eff1cb5d0302ce2c9c6af15abc1e0d364340bd33
MonsterWeed <56317119+MonsterWeed@users.noreply.github.com>
2019-10-19 16:03:15 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on("message", (message) => {
   if (message.content===("!puestada")) {
     message.channel.send("con toda la puestada con toda la puestada ayer pille dossss y hoy otros doossss",{files:["./image/puestadaa.gif"]});
   }
-  /*if (message.content.startsWith("!capitanes")&&message.content.endsWith("!capitanes")){
+  /*if (message.content===("!capitanes")){
     var random1=Math.floor(Math.random() * 10)+1;  
     var random2=Math.floor(Math.random()*10)+1;
     while (random1==random2){random2=Math.floor(Math.random()*10)+1;}
@@ -42,21 +42,21 @@ case 10:message.channel.send("Adox"+" (Comienza eligiendo miembros)");break;
    }
   }*/
 
-if (message.content.startsWith("!capitanes")&&message.content.endsWith("!capitanes")){
+if (message.content===("!capitanes")){
   message.channel.send("***\nEl comando !capitanes NO estara disponible hasta el 26/10/2019***");
 }
 
-  if (message.content.startsWith("!patada")&&message.content.endsWith("!patada")){
+  if (message.content===("!patada")){
     message.channel.send({files:["./image/tinnico.gif"]});
   }
-  if(message.content.startsWith("!judio")&&message.content.endsWith("!judio")){
+  if(message.content===("!judio")){
     message.channel.send("Como vas a decir eso judio hijo de puta!!",{files:["./image/judio.gif"]});
   }
-   if(message.content.startsWith("!inflacion")&&message.content.endsWith("!inflacion")){
+   if(message.content===("!inflacion")){
   message.channel.send( "La inflacion es siempre y en todo lugar un FENOMENO MONETARIO",{files:["./image/milei.jpg"]});
 }
 
-  if (message.content.startsWith("!comandos")&&message.content.endsWith("!comandos")){
+  if (message.content===("!comandos")){
     message.channel.send({embed: {
       color: 3447003,
       description: "__Los comandos actuales en el servidor son:__\n\n**!puestada\n!baja\n!inflacion\n!patada\n!host\n!limpiar\n!capitanes (solo disponible el 26/10/2019)\n!torneo\n!judio**"
@@ -64,32 +64,32 @@ if (message.content.startsWith("!capitanes")&&message.content.endsWith("!capitan
 
   }
 
-  if (message.content.startsWith("!torneo")&&message.content.endsWith("!torneo")){
+  if (message.content===("!torneo")){
     message.channel.send("\n__**El torneo se llevara a cabo el dia 26/10/2019**__"+"\n\n__Integrantes Confirmados:__"+"\nSanti\ngeno\nTinnico\nMonsterWeed\nbranca\nTuki\nEmiliano\nclocken\nMax\nAdox"+"\n\n__Suplentes:__\nTiti\n2puntosbcorta");
   }
    
-   if (message.content.startsWith("!host")&&message.content.endsWith("!host")){
+   if (message.content===("!host")){
     message.channel.send("\n__**Mi creador MonsterWeed ya me hosteo y ahora soy mas rapido y eficaz :yum:**__");
   }
    
-   if(message.content.startsWith("!baja")&&message.content.endsWith("!baja")){
+   if(message.content===("!baja")){
   message.channel.send({files:["./image/baja.gif"]});
 }  
    
-if(message.content.startsWith("!limpiar")&&message.content.endsWith("!limpiar")){
+if(message.content===("!limpiar")){
   message.channel.send("limpiando... \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPantalla Limpia :page_facing_up: ");
   }
    
    
 //comunicadoss
    /*
-  if(message.content.startsWith("!das")){
+  if(message.content===("!das")){
     message.channel.send("[LNDC]©                                                  13/10/2019\n\n"+
     "La administracion se complace en presentar el canal de Updates del servidor.\n"
     +"En esta seccion se publicara las modificaciones que se le hagan al servidor.\n\n"
     +"**Este canal es exclusivamente para publicaciones de la administracion por lo tanto permanecera cerrado(solo lectura).**\n@here");
   }
- if (message.content.startsWith("!asd")){
+ if (message.content===("!asd")){
     message.channel.send("[LNDC]©                                                  13/10/2019\n\n"+
     "__**Actualizacion v1.1:**__\n"+
     "-Nuevo comando de **!limpiar**. Este comando es util para aquellos que desean limpiar el chat cuando alguien manda algun tipo de material comprometido de instagram o twitter dentro del canal de mensajes.\n\n"+
@@ -98,7 +98,7 @@ if(message.content.startsWith("!limpiar")&&message.content.endsWith("!limpiar"))
    
    //nuevos comandos (Alpha)
  
-if(message.content.startsWith("!lindo")&&message.content.endsWith("!lindo")){
+if(message.content===("!lindo")){
     message.channel.send("Segun "+message.author.toString()+ " el bebe mas hermoso de este servidor es:");
     var ran=Math.floor(Math.random() * 7)+1;
     switch(ran){
@@ -112,7 +112,7 @@ if(message.content.startsWith("!lindo")&&message.content.endsWith("!lindo")){
      }
   }
    
- if (message.content.startsWith("!test")){
+ if (message.content===("!test")){
     message.channel.send(message.author.toString()+"si salio");
   }
  
