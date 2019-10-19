77f334a3034965a6e08274fd9671f748dd72409c
MonsterWeed <56317119+MonsterWeed@users.noreply.github.com>
2019-10-19 16:31:19 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -113,8 +113,9 @@ if(message.content===("!limpiar")){
   }*/
    
  if (message.content===("!test")){
-    message.channel.send(message.author.toString()+"si salio");
      message.delete();
+    message.channel.send(message.author.toString()+"si salio");
+     
   }
  
    
