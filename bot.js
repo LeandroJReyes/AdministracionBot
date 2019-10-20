26227bad6cb9e536c9b808938c3bf8cbbc6b652c
MonsterWeed <56317119+MonsterWeed@users.noreply.github.com>
2019-10-20 13:18:42 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -234,7 +234,7 @@ if(message.content===("!f")){
    
 if (message.content.startsWith("!")&&message.content!==("!comandos")&&message.content!==("!baja")&&message.content!==("!puestada")&&message.content!==("!inflacion")&&message.content!==("!patada")
   &&message.content!==("!host")&&message.content!==("!limpiar")&&message.content!==("!capitanes")&&message.content!==("!torneo")&&message.content!==("!judio")&&message.content!==("!test")&&message.content!==("!sale")&&message.content!==("!lanz")
-  &&message.content!==("!F")&&message.content!==("!f")&&!message.content.startsWith("!!!!")&&message.content!==("!lindo")&&message.content!==("!!!!")){
+  &&message.content!==("!F")&&message.content!==("!f")&&message.content!==("!lindo")&&message.content==("!!!")){
      message.delete();
     message.channel.send(message.author.toString()+" __**No se reconocio el comando,por favor compruebe el comando usando !comandos **__");
   }
