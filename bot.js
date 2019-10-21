839e23411304a519da1004f680a75f7b55e21fae
MonsterWeed <56317119+MonsterWeed@users.noreply.github.com>
2019-10-21 14:25:00 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -209,10 +209,10 @@ if (message.content===("!lanz")){
    message.delete();
  message.channel.send("**La administracion ya sorteo los capitanes y ya se formaron los equipos, que son los siguientes:**\n\n"+
  "__Team Mancoman__                                      __Team Tuki__\n"+
- "    Clocken                                                MonsterWeed      \n"+
- "    Branca                                                  Geno        \n"+
- "    Santi                                                    Tinnico    \n"+
- "    Adoxx                                                  Max "); 
+ "    Clocken                                                MonsterWeed\n"+
+ "    Branca                                                  Geno\n"+
+ "    Santi                                                    Tinnico\n"+
+ "    Adoxx                                                  Max"); 
 }
    
 if (message.content.startsWith("!")&&message.content!==("!comandos")&&message.content!==("!baja")&&message.content!==("!puestada")&&message.content!==("!inflacion")&&message.content!==("!patada")
