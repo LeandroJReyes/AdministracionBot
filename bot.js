5e156631301aab286515d2b8d2de354578dec51d
MonsterWeed <56317119+MonsterWeed@users.noreply.github.com>
2020-08-02 20:50:25 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,14 +26,12 @@ try{
 if(receivedMessage.startsWith("!")&&!receivedMessage.endsWith("!"))
 {
   receivedMessage.author.send("No se puede utilizar comandos del servidor en Mensajes privados.");
- 
 }
-else
-{
+
 var chanelmensaje=client.channels.get("739617321446014997");
 chanelmensaje.send("De: "+receivedMessage.author.toString()+"\nMensaje: "+receivedMessage.toString());
 receivedMessage.author.send("Mensaje Recibido con exito!");
-}
+
 }
 catch(error)
 {receivedMessage.author.send("Error de mensajeee");}
