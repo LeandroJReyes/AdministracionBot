40ba8d4287dd21af94b2439bb5a3766c2b6c9d23
MonsterWeed <56317119+MonsterWeed@users.noreply.github.com>
2020-08-02 01:02:00 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on("ready", () => {
 
   var generalChannel = client.channels.get("587678594525036568"); // Replace with known channel ID
   generalChannel.send("Volvi a estar Online manga de cornetas, me extrañaron?");  
-  generalChannel.message.react('❌');
-  generalChannel.message.react('✅');
+  generalChannel.react('❌');
+  //generalChannel.message.react('✅');
 });
 
 
