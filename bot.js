e475671611471c83d7beee1018c98ac16c5985d3
MonsterWeed <56317119+MonsterWeed@users.noreply.github.com>
2020-08-04 22:36:28 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,7 +55,7 @@ if(message.channel.type!="dm")
       if (message.member.voiceChannel) 
       {
         message.member.voiceChannel.join().then(connection => {
-          const dispatcher = connection.playFile("./image/puta.mp3");
+          const dispatcher = connection.playFile("./image/Mafia.mp3");
           dispatcher.on('end', end => message.member.voiceChannel.leave());
         });
       }
