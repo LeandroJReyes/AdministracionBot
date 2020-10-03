bdd6f25e1c3ac0c1c8fdcae2397fc4406418859e
MonsterWeed <56317119+MonsterWeed@users.noreply.github.com>
2020-10-03 14:37:17 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,6 +13,11 @@ client.on("ready", () => {
     sentMessage.react('✅');
     sentMessage.react('❌');
   });
+  var cannelqueja = client.channels.get("479096159198117888");
+  generalChannel.send("03/10/2020                              [LNDC]\n \t La solicutud ha sido recibida con exito y la administracion ha decidido abrir una votacion para la postulacion de Pachu.\nLa votacion cerrara el dia 11/10/2020.").then(sentMessage => {
+    sentMessage.react('✅');
+    sentMessage.react('❌');
+  });
   /*generalChannel2.send("Volvi a estar Online manga de cornetas, me extrañaron?").then(sentMessage => {
     sentMessage.react('✅');
     sentMessage.react('❌');
